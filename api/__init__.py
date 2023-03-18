9c60a65db964c81bd4306378806a440487e24c5d
Bidemi saka <bidemisaka2000@gmail.com>
2023-03-18 22:06:36 +0100

    slight change commit

--- api/__init__.py
+++ api/__init__.py
@@ -18,10 +18,20 @@ def create_app(config=config_dict['dev']):
     db.init_app(app)
 
     migrate = Migrate(app, db)
-
+    authorizations = {
+        "Bearer Auth": {
+            'type': "apikey",
+            'in': 'header',
+            'name': "Authorization",
+            'description': "Add a jwt with ** Bearer &lt;JWT&gt; to authorize"
+        }
+    }
     api = Api(app,
               title="student-Management-system",
-              description="A REST API for Student Management")
+              description="A REST API for Student Management",
+              authorizations=authorizations,
+              security="Bearer Auth"
+              )
 
     api.add_namespace(student_namespace, path='/student')
     api.add_namespace(course_namespace, path='/course')
